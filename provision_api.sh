bea4ce57998c961beb674508ea7fbbb0a8ea3b13
kim-ji011 <rlawldn.0511@gmail.com>
2025-09-06 15:27:57 +1200

    Fix paths in provisioning scripts for API and static files

--- provision_api.sh
+++ provision_api.sh
@@ -5,7 +5,7 @@ apt-get install -y nodejs
 npm install -g pm2
 cd /vagrant/app
 npm install
-pm2 start server.js --name bird-api
+pm2 start /vagrant/server.js --name bird-api
 sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u vagrant --hp /vagrant
 pm2 save
 echo "API provisioned and running on port 3000."
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 /* host public/ directory to serve: images, css, js, etc. */
-app.use(express.static('public'));
+app.use(express.static('/vagrant/public'));
 
 /* path routing and endpoints */
 app.use('/', require('./path_router'));
